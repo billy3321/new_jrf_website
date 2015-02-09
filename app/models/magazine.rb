9414b0f9e12aedc875699306fc26a38485fbec44
billy3321 <billy3321@gmail.com>
2015-02-09 11:11:11 +0800

    add sort and some new field

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,7 +2,7 @@ class Magazine < ActiveRecord::Base
   has_many :magazine_articles
   has_many :columns, through: :magazine_articles
   before_save :update_name
-  default_scope { order(created_at: :desc) }
+  default_scope { order(published_at: :desc) }
   validates_presence_of :issue, message: '請填寫雜誌期數'
   validates_presence_of :published_at, message: '請填寫出版日期'
 
--- app/models/magazine_article.rb
+++ app/models/magazine_article.rb
@@ -2,4 +2,9 @@ class MagazineArticle < ActiveRecord::Base
     has_and_belongs_to_many :keywords, -> { uniq }
     belongs_to :magazine
     belongs_to :column
+
+    default_scope {
+      includes(:magazine).
+      order("magazines.published_at DESC")
+    }
 end
