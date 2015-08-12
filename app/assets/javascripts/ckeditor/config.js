a21ee235d7f83b7d6544a6521cff7936d295cbe6
billy3321 <billy3321@gmail.com>
2015-08-12 18:05:51 +0800

    add ;

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,5 +2,5 @@ if (typeof(CKEDITOR) != undefined) {
     CKEDITOR.editorConfig = function (config) {
         config.extraAllowedContent = 'i dl dt dd';
     }
-    CKEDITOR.dtd.$removeEmpty['i'] = false
+    CKEDITOR.dtd.$removeEmpty['i'] = false;
 }
