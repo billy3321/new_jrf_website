f9e91388bcf6c86ca647aa0aa2b537c420e61519
billy3321 <billy3321@gmail.com>
2015-08-12 17:48:29 +0800

    add more item support

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,6 @@
 if (typeof(CKEDITOR) != undefined) {
     CKEDITOR.editorConfig = function (config) {
-        config.extraAllowedContent = 'i'
+        config.extraAllowedContent = 'i dl dt dd';
     }
     CKEDITOR.dtd.$removeEmpty['i'] = false
 }
