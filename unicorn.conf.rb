79dc4b260c9f9051765aac75129975bf11510b22
billy3321 <billy3321@gmail.com>
2015-12-08 14:56:57 +0800

    fix css and add worker_processes

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,4 +1,5 @@
 working_directory ""
+worker_processes 4
 listen "/var/run/unicorn/jrf_website_unicorn.sock", :backlog => 64
 pid "/var/run/unicorn/jrf_website_unicorn.pid"
 stderr_path "/var/log/jrf_website_unicorn.stderr.log"
